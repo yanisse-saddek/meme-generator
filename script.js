1d3e55f5495e3b588411d240b9434d4ddfedd25b
yanisse-saddek <yanisse321@gmail.com>
2022-04-30 09:25:07 +0200

    update

--- script.js
+++ script.js
@@ -2,36 +2,43 @@ var test = $('.ok');
 var canvas = $('#myCanvas');
 var ctx = canvas[0].getContext("2d");
 var background = new Image();
-var top = 0;
-var bottom = 0;
+var topColor = "black"
+var bottomColor = "orange"
+var textBottom = ""
+var textTop = ""
+var fontSize = 30;
+var font = "Comic Sans MS"
 
-ctx.font = "bold 30px arial";
-ctx.textAlign="center"
 
 $('#valid-upload').click((e)=>{
     background.src= $('#upload-link').val()
 })
-background.src = "https://media.discordapp.net/attachments/946724370338943017/963369945893335050/unknown.png";
+background.src = "https://media.discordapp.net/attachments/946724370338943017/968449899928616960/Snapchat-71237284.jpg?width=447&height=596";
 background.onload = function(){
-    ctx.drawImage(background,0,0, canvas.width(), canvas.height());   
+    canvas[0].width=background.width
+    canvas[0].height=background.height
+    ctx.drawImage(background,0,0, canvas.width(), canvas.height());  
+    console.log(canvas.height()-(canvas.height()-10)) 
+    
+    ctx.font = fontSize+'px '+font;
+    ctx.textAlign="center"
 }
+
 
 $('#top').keyup((e)=>{
     ctx.clearRect(0, 0, canvas.width(), canvas.height()/2);
-    drawText(e.target.value,"black", 40, 'top');
+    drawText(e.target.value,topColor, canvas.height()-(canvas.height()-fontSize), 'top');
 })
 $('#bottom').keyup((e)=>{
     ctx.clearRect(0, canvas.height()/2, canvas.width(), canvas.height()/2);
-    drawText(e.target.value,"black", 480, 'bottom');
+    drawText(e.target.value,bottomColor, canvas.height()-fontSize, 'bottom');
 })
 
 function drawText(text,fill, dir, textDirection){
-    if(textDirection === 'top'){
+    if(textDirection == 'top'){
         textTop = text
-        top=dir
     }else{
         textBottom=text
-        bottom=dir
     }
     ctx.fillStyle=fill;
     ctx.lineWidth=4;
@@ -39,9 +46,18 @@ function drawText(text,fill, dir, textDirection){
     save()
 }
 
-
 function save(){
         ctx.drawImage(background,0,0, canvas.width(), canvas.height());   
-        ctx.fillText(textTop.toUpperCase(),canvas.width()/2, 40);
-        ctx.fillText(textBottom.toUpperCase(),canvas.width()/2, bottom);
+        if(textTop){
+            ctx.fillStyle=topColor;
+            ctx.fillText(textTop.toUpperCase(),canvas.width()/2, canvas.height()-(canvas.height()-fontSize));
+        }
+        if(textBottom){
+            ctx.fillStyle=bottomColor;
+            ctx.fillText(textBottom.toUpperCase(),canvas.width()/2, canvas.height()-fontSize);
+        }
+}
+
+function changeColor(id){
+
 }
