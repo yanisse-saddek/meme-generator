32fb25d6368a7d5a1cd87eec6d4bbc552a3e2377
yanisse-saddek <yanisse321@gmail.com>
2022-04-30 15:32:31 +0200

    update

--- script.js
+++ script.js
@@ -16,27 +16,22 @@ var lineheight = 25;
 $('#valid-upload').click((e)=>{
     background.src= $('#upload-link').val()
 })
-// background.src = "https://media.discordapp.net/attachments/946724370338943017/968449899928616960/Snapchat-71237284.jpg?width=447&height=596";
+background.src = "https://media.discordapp.net/attachments/946724370338943017/968449899928616960/Snapchat-71237284.jpg?width=447&height=596";
 background.onload = function(){
     canvas[0].width=background.width
     canvas[0].height=background.height
     ctx.drawImage(background,0,0, canvas.width(), canvas.height());  
-    console.log(canvas.height()-(canvas.height()-10)) 
-    
+    x = canvas.width()/2;
+    y = canvas.height();
+
     ctx.font = fontSize+'px '+font;
     ctx.textAlign="center"
-     x = canvas.width()/2;
-     y = canvas.height();
-     lineheight = 25;
-     
 }
 
 
 $('#top').keyup((e)=>{
-    ctx.clearRect(0, 0, canvas.width(),canvas.width(), canvas.height()/2);
-    var msg = e.target.value
-
-    drawText(msg,topColor, y-(y-fontSize), 'top');
+    ctx.clearRect(canvas.width()/2, 0, canvas.width(), canvas.height()/2);
+    drawText(e.target.value,topColor, y-(y-fontSize), 'top');
 })
 
 $('#bottom').keyup((e)=>{
@@ -44,30 +39,58 @@ $('#bottom').keyup((e)=>{
     drawText(e.target.value,bottomColor, canvas.height()-fontSize, 'bottom');
 })
 
-function drawText(text,fill, dir, textDirection){
 
-    if(textDirection == 'top'){
-        textTop = text
-    }else{
-        textBottom=text
+var dataTop = []
+var dataBottom = []
+function drawText(text,fill, dir, textDirection){
+    
+    var txt = text.split('')
+    var count = 0
+    for(j=0; j<txt.length; j++){
+        if(count<20){
+            count++
+        }else if (txt[j]==" "){
+            txt.splice(j, 0, "°");
+            count=0
+        }   
     }
-    ctx.fillStyle=fill; 
-    ctx.lineWidth=4;
-    ctx.fillText(text.toUpperCase(),x,dir);
+    txt = txt.join('')
+    lines = txt.split('°');
+    for (var i = 0; i<lines.length; i++){
+        iLength = lines.length
+        ctx.fillStyle=fill; 
+        ctx.lineWidth=4;   
+
+        if(textDirection == 'top'){
+            dataTop[i] = []
+            textTop = text
+            var actualX = (i*fontSize)+30
+            dataTop[i] = [lines[i], actualX, 'top']
+            ctx.fillText(lines[i],x,actualX);
+        }else{
+            dataBottom[i] = []
+            textBottom=text
+            iLength-i
+            var actualY = y-(iLength*fontSize)+(i*30)
+            dataBottom[i] = [lines[i], actualY, 'bottom']
+            ctx.fillText(lines[i],x, actualY);
+        }
+    } 
     save()
 }
-
-function save(){
-        ctx.drawImage(background,0,0, canvas.width(), canvas.height());   
-        
-        if(textTop){
-            ctx.fillStyle=topColor;
-            ctx.fillText(textTop.toUpperCase(),x, y-(y-fontSize));
-        }
-        if(textBottom){
+function save(lines){
+    console.log(dataBottom)
+    ctx.drawImage(background,0,0, canvas.width(), canvas.height());  
+    dataBottom.map(line=>{
             ctx.fillStyle=bottomColor;  
-            ctx.fillText(textBottom.toUpperCase(),x, y-fontSize);
-        }
+            ctx.fillText(line[0].toUpperCase(),x, line[1]);
+    })
+    dataTop.map(line=>{
+        ctx.fillStyle=topColor;
+        ctx.fillText(line[0].toUpperCase(),x, line[1]);
+
+    })
+
 }
 
 function changeColor(id){
@@ -78,6 +101,12 @@ function changeColor(id){
         $('.top-color').css('background', data[1])
     }else{
         bottomColor=data[1]
+        $('.bottom-color').css('background', data[1])
     }
     save()
+}
+function openColor(id){
+    console.log(id)
+    console.log('oki')
+    $(`#${id}`).fadeToggle()
 }
